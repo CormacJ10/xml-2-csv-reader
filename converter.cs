d9bdf07aa42e917d722c5f97b003317190e499d7
Joshua Jimenez <30546138+CormacJ10@users.noreply.github.com>
2021-11-20 12:31:01 +1300

    initial commit

---
+++ converter.cs
@@ -0,0 +1,22 @@
+using System.IO;
+using System.Xml.Serialization;
+
+using (FileStream fs = new FileStream(@"D:\youXMLFile.xml", FileMode.Open))
+{
+    XmlSerializer serializer = new XmlSerializer(typeof(Sequence[]));
+    var data=(Sequence[]) serializer.Deserialize(fs);
+    List<string> list = new List<string>();
+    foreach(var item in data)
+    {
+        List<string> ss = new List<string>();
+        foreach (var point in item.SourcePath) ss.Add(point.X + "," + point.Y);
+        list.Add(string.Join(",", ss));
+    }
+    File.WriteAllLines("D:\\csvFile.csv", list);
+}
+
+public class Sequence
+{
+    public Point[] SourcePath { get; set; }
+}
+
