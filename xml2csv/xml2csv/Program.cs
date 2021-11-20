b52de84f450972e366cc912cee0bd203cee0d5e4
Joshua Jimenez <30546138+CormacJ10@users.noreply.github.com>
2021-11-20 22:35:26 +1300

    solution build

---
+++ xml2csv/xml2csv/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.IO;
+using System.Xml.Serialization;
+
+namespace xml2csv
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
