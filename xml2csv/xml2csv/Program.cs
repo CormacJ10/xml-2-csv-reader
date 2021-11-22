b45daf423af80307578d7d1567e6859234f83add
Joshua Jimenez <30546138+CormacJ10@users.noreply.github.com>
2021-11-22 22:52:55 +1300

    final commit

--- xml2csv/xml2csv/Program.cs
+++ xml2csv/xml2csv/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Drawing;
 using System.IO;
 using System.Xml.Serialization;
 
@@ -11,4 +12,25 @@ namespace xml2csv
             Console.WriteLine("Hello World!");
         }
     }
+
+    using (FileStream fs = private new FileStream("testfile.xml", FileMode.Open)){
+        XmlSerializer serializer = new XmlSerializer(typeof(Sequence[]));
+        var data = (Sequence[])serializer.Deserialize(fs);
+        List<string> list = new List<string>();
+        foreach (var item in data)
+        {
+            List<string> ss = new List<string>();
+            foreach (var point in item.SourcePath) ss.Add(point.X + "," + point.Y);
+            list.Add(string.Join(",", ss));
+        }
+        File.WriteAllLines("export.csv", list);
+    }
+
+    public class Sequence
+    {
+        public Point[] SourcePath { get; set; }
+    }
+
 }
+
+
